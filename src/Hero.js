b7811988aa522bec1f5de3c83d55e4504e24b2dd
Lohit Dmello <lohit56@gmail.com>
2021-09-12 11:11:27 +0530

    adding project to git

---
+++ src/Hero.js
@@ -0,0 +1,12 @@
+import "./Hero.css";
+import Bg from "./img/bg.svg";
+function Hero(props) {
+  return (
+    <div className="hero">
+      {/* <div className="bg">
+        <img src={Bg} className="bg-img" alt="svg" />
+      </div> */}
+    </div>
+  );
+}
+export default Hero;
